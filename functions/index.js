6dbad656e4311a38a54141d0c4aba023b5e276a6
Anupam Mijar <aamijar230@gmail.com>
2021-02-03 01:11:45 -0500

    refactor: pull 2021 events

--- functions/index.js
+++ functions/index.js
@@ -25,7 +25,10 @@ const constants = require('./constants');
 const user = constants.user;
 const pass = constants.pass;
 
-const baseURL = 'https://frc-api.firstinspires.org/v2.0/2020/';
+const baseURL = 'https://frc-api.firstinspires.org/v2.0/2021/';
+
+const rootCollection = "2021_data"
+
 
 //setup
 admin.initializeApp();
@@ -58,8 +61,8 @@ exports.updateFirestore = functions.runWith(runtimeOpts).https.onRequest((reques
 
   }).then((finalResult) => {
     console.log(finalResult);
-    return pubSubClient.topic('districtStart').publishJSON({"district": "start"});
-    //return pubSubClient.topic('regionalStart').publishJSON({"regional": "start"});
+    //return pubSubClient.topic('districtStart').publishJSON({"district": "start"});
+    return pubSubClient.topic('regionalStart').publishJSON({"regional": "start"});
     //return pubSubClient.topic('districtOverflow').publishJSON([{"name": "myname", "number": 0}, {"name": "myname2", "number": 2}]);
     
   }).then((pubsubMessage) => {
@@ -230,7 +233,7 @@ function getChampionships(){
             }   
         }
 
-        admin.firestore().collection("cloud functions").doc('championships').set({"championshipCodes": championshipArray});
+        admin.firestore().collection(rootCollection).doc('championships').set({"championshipCodes": championshipArray});
         
         resolve(championshipArray);
         
@@ -340,7 +343,7 @@ function getChampionshipAvatars(tempChampionshipObject){
         }
         
 
-        admin.firestore().collection('cloud functions').doc('championships').collection('teams').doc(code).set({"teams": teamNames});
+        admin.firestore().collection(rootCollection).doc('championships').collection('teams').doc(code).set({"teams": teamNames});
         resolve(code + " > teams list fetched!");
 
       }
@@ -391,7 +394,7 @@ function getRegionals(){
             }   
         }
 
-        admin.firestore().collection("cloud functions").doc('regionals').set({"regionalCodes": regionalArray});
+        admin.firestore().collection(rootCollection).doc('regionals').set({"regionalCodes": regionalArray});
         
 
         resolve(regionalArray);
@@ -490,7 +493,7 @@ function getRegionalAvatars(tempRegionalObject){
         }
         
 
-        admin.firestore().collection('cloud functions').doc('regionals').collection('teams').doc(code).set({"teams": teamNames});
+        admin.firestore().collection(rootCollection).doc('regionals').collection('teams').doc(code).set({"teams": teamNames});
         resolve(code + " > teams list fetched!");
 
       }
@@ -540,7 +543,7 @@ function getDistricts(){
 
         
         
-        admin.firestore().collection('cloud functions').doc('districts').set({"districtCodes": districtArray});
+        admin.firestore().collection(rootCollection).doc('districts').set({"districtCodes": districtArray});
         
 
         resolve(districtArray);
@@ -599,7 +602,7 @@ function getDistrictEvents(districtCode){
         tempEventObject.events = eventsArray;
         
         
-        admin.firestore().collection('cloud functions').doc('districts').collection('district events').doc(districtCode).set({"district event": eventsArray})
+        admin.firestore().collection(rootCollection).doc('districts').collection('district events').doc(districtCode).set({"district event": eventsArray})
         
         
         resolve(tempEventObject);
@@ -711,7 +714,7 @@ function getDistrictAvatars(eventObject){
         }
         
 
-        admin.firestore().collection('cloud functions').doc('districts').collection('district events').doc(districtCode).collection('teams').doc(eventCode).set({"teams": teamNames})
+        admin.firestore().collection(rootCollection).doc('districts').collection('district events').doc(districtCode).collection('teams').doc(eventCode).set({"teams": teamNames})
         resolve(eventCode + " " + districtCode + " > team list fetched!");
 
       }
@@ -740,7 +743,7 @@ function simpleTest(){
 
 /* test function for getting data from firestore */
 async function getData(){
-  const regionalRef = admin.firestore().collection('cloud functions').doc('regionals');
+  const regionalRef = admin.firestore().collection(rootCollection).doc('regionals');
   const doc = await regionalRef.get();
   if (!doc.exists) {
     console.log('No such document!');
